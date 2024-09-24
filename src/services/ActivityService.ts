9d4e0187f341f520c54fe2f265634a49f991f9a1
javierspr <javxdev@gmail.com>
2024-09-24 11:08:54 -0300

    add safeParse to addActivity

--- src/services/ActivityService.ts
+++ src/services/ActivityService.ts
@@ -1,9 +1,13 @@
 import axios from "axios"
 import { Activity } from "../types"
+import { ActivitySchema } from "../types";
 
 export const addActivity = async (data: Activity) => {
     try {
-        await axios.post(`${import.meta.env.VITE_API_URL}/api/activities`, data);
+        const result = ActivitySchema.safeParse(data)
+        if(result.success){
+            await axios.post(`${import.meta.env.VITE_API_URL}/api/activities`, result.data);
+        }
     } catch (error) {
         throw error;
     }
