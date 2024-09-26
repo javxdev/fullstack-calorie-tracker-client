813d62bd7735bf651515de79809f8fbe05a63dca
javierspr <javxdev@gmail.com>
2024-09-26 12:58:08 -0300

    add save-activity action

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,10 +1,7 @@
 import { Activity } from "../types"
-import { addActivity } from "../services/ActivityService"
 
 export type ActivityActions =
-    { type: 'save-activity', payload: { newActivity: Activity } } |
-    // { type: 'set-activeId', payload: { id: Activity['id'] } } |
-    // { type: 'delete-activity', payload: { id: Activity['id'] } }|
+    { type: 'save-activity', payload: { activities: Activity[] } } |
     { type: 'restart-app' } 
 
 
@@ -12,14 +9,9 @@ export type ActivityState = {
     activities: Activity[]
 }
 
-const localStorageActivities = () : Activity[] => {
-    const activities = localStorage.getItem('activities')
-    return activities ? JSON.parse(activities) : []
-}
 
 export const initialState : ActivityState  = {
-    activities: localStorageActivities(),
-    // activeId: 0
+    activities: []
 }
 
 export const activityReducer = (
@@ -28,27 +20,11 @@ export const activityReducer = (
     ) => {
     
     if(action.type === 'save-activity') {
-        addActivity(action.payload.newActivity)
-
         return {
-            ...state
+            ...state,
+            activities: action.payload
         }
     }
-    
-    // if(action.type === 'set-activeId'){
-        
-    //     return {
-    //         ...state,
-    //         activeId: action.payload.id
-    //     }
-    // }
-
-    // if(action.type === 'delete-activity') {
-    //     return {
-    //         ...state,
-    //         activities: state.activities.filter( activity => activity.id !== action.payload.id )
-    //     }
-    // }
 
     if(action.type === 'restart-app') {
         return {
